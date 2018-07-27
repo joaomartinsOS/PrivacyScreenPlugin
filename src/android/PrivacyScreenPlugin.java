7164b7a8ae8463016ef79199d83a1e7f3284f485
João Martins <joao.martins@outsystems.com>
2018-07-27 17:38:25 +0100

    test OS

--- src/android/PrivacyScreenPlugin.java
+++ src/android/PrivacyScreenPlugin.java
@@ -6,75 +6,67 @@
  */
 package org.devgeeks.privacyscreen;
 
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaInterface;
-import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.PluginResult;
-
 import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.util.Log;
-import android.view.Window;
 import android.view.WindowManager;
 
-
-import org.json.JSONArray;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
 import org.json.JSONException;
-import org.json.JSONObject;
-
-import android.content.Context;
-import android.os.Bundle;
 
 /**
  * This class sets the FLAG_SECURE flag on the window to make the app
  * private when shown in the task switcher
  */
 public class PrivacyScreenPlugin extends CordovaPlugin {
-	private static final String CHANGE_PRIVACY = "changePrivacy";
-	public static final String KEY_PRIVACY_SCREEN_ENABLED = "org.devgeeks.privacyscreen/PrivacyScreenEnabled";
-	private SharedPreferences preferences;
-	public CallbackContext callbackContext;
-	Activity activity;
-	WindowManager.LayoutParams params;
-	WindowManager wm;
-	CordovaWebView currView;
+    private static final String CHANGE_PRIVACY = "changePrivacy";
+    public static final String KEY_PRIVACY_SCREEN_ENABLED = "org.devgeeks.privacyscreen/PrivacyScreenEnabled";
+    private SharedPreferences preferences;
+    public CallbackContext callbackContext;
+    private Activity activity;
+    private WindowManager.LayoutParams params;
+    private WindowManager wm;
+    private CordovaWebView currView;
 
-  @Override
-  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-    super.initialize(cordova, webView);
-    activity = this.cordova.getActivity();
-	params = activity.getWindow().getAttributes();
-    preferences = PreferenceManager.getDefaultSharedPreferences(activity);
-    boolean privacyScreenEnabled = isPrivacyScreenEnabled(true);
-	wm = this.cordova.Context.getSystemService(Context.WINDOW_SERVICE);
-	currView = webView;
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        activity = this.cordova.getActivity();
+        params = activity.getWindow().getAttributes();
+        preferences = PreferenceManager.getDefaultSharedPreferences(activity);
+        boolean privacyScreenEnabled = isPrivacyScreenEnabled(true);
+        /*wm = this.cordova.Context.getSystemService(Context.WINDOW_SERVICE);*/
 
-    if (privacyScreenEnabled) {
-      activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
-	  
+       /* currView = webView;*/
+
+        if (privacyScreenEnabled) {
+            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+
+        }
     }
-  }
-  
+
 
     /**
      * Executes the request and returns PluginResult.
      *
-     * @param args              JSONArry of arguments for the plugin.
-     * @param callbackContext   The callback id used when calling back into JavaScript.
-     * @return                  A PluginResult object with a status and message.
+     * @param args            JSONArry of arguments for the plugin.
+     * @param callbackContext The callback id used when calling back into JavaScript.
+     * @return A PluginResult object with a status and message.
      */
-  public boolean execute(String action, boolean arg, CallbackContext callbackContext ) throws JSONException {
+    public boolean execute(String action, boolean arg, CallbackContext callbackContext) throws JSONException {
         this.callbackContext = callbackContext;
-		
-		if (action.equals(CHANGE_PRIVACY)) {
+
+        if (action.equals(CHANGE_PRIVACY)) {
             try {
                 setPrivacyScreenEnabled(arg);
-				actualyChangeFlag();
-            }
-            catch (Exception e)
-            {
+                actualyChangeFlag();
+            } catch (Exception e) {
                 callbackContext.error("Error");
                 PluginResult r = new PluginResult(PluginResult.Status.ERROR);
                 callbackContext.sendPluginResult(r);
@@ -83,119 +75,117 @@ public class PrivacyScreenPlugin extends CordovaPlugin {
         } else {
             return false;
         }
-		PluginResult r = new PluginResult(PluginResult.Status.NO_RESULT);
+        PluginResult r = new PluginResult(PluginResult.Status.NO_RESULT);
         r.setKeepCallback(true);
         callbackContext.sendPluginResult(r);
-		return true;
+        return true;
     }
 
-  /**
-   * Gets the value of the Privacy Screen Enabled entry stored in {@link SharedPreferences}.
-   * Will not attempt to correct an erroneous entry.
-   *
-   * @param defValue The default value of the preference if not readable or non-existent.
-   * @return Whether the privacy screen should be enabled during the next application launch.
-   * @see #KEY_PRIVACY_SCREEN_ENABLED
-   * @see #isPrivacyScreenEnabled(boolean, boolean)
-   */
-  private boolean isPrivacyScreenEnabled(boolean defValue) {
-    return isPrivacyScreenEnabled(defValue, false);
-  }
-
-  /**
-   * Gets the value of the Privacy Screen Enabled entry stored in {@link SharedPreferences}.
-   *
-   * @param defValue      The default value of the preference if not readable or non-existent.
-   * @param shouldCorrect If true, will attempt to convert String value to Boolean or replace with the default value.
-   * @return Whether the privacy screen should be enabled during the next application launch.
-   * @see #KEY_PRIVACY_SCREEN_ENABLED
-   */
-  private boolean isPrivacyScreenEnabled(boolean defValue, boolean shouldCorrect) {
-    if (!preferences.contains(KEY_PRIVACY_SCREEN_ENABLED)) {
-      setPrivacyScreenEnabled(defValue);
-      return defValue;
+    /**
+     * Gets the value of the Privacy Screen Enabled entry stored in {@link SharedPreferences}.
+     * Will not attempt to correct an erroneous entry.
+     *
+     * @param defValue The default value of the preference if not readable or non-existent.
+     * @return Whether the privacy screen should be enabled during the next application launch.
+     * @see #KEY_PRIVACY_SCREEN_ENABLED
+     * @see #isPrivacyScreenEnabled(boolean, boolean)
+     */
+    private boolean isPrivacyScreenEnabled(boolean defValue) {
+        return isPrivacyScreenEnabled(defValue, false);
     }
-    try {
-      return preferences.getBoolean(KEY_PRIVACY_SCREEN_ENABLED, defValue);
-    } catch (ClassCastException e) {
-      Log.w("PrivacyScreen", "SharedPreference '" + KEY_PRIVACY_SCREEN_ENABLED + "' was not a Boolean value.", e);
 
-      if (shouldCorrect) {
-        if (convertStringEntryToBoolean()) {
-          return preferences.getBoolean(KEY_PRIVACY_SCREEN_ENABLED, defValue);
+    /**
+     * Gets the value of the Privacy Screen Enabled entry stored in {@link SharedPreferences}.
+     *
+     * @param defValue      The default value of the preference if not readable or non-existent.
+     * @param shouldCorrect If true, will attempt to convert String value to Boolean or replace with the default value.
+     * @return Whether the privacy screen should be enabled during the next application launch.
+     * @see #KEY_PRIVACY_SCREEN_ENABLED
+     */
+    private boolean isPrivacyScreenEnabled(boolean defValue, boolean shouldCorrect) {
+        if (!preferences.contains(KEY_PRIVACY_SCREEN_ENABLED)) {
+            setPrivacyScreenEnabled(defValue);
+            return defValue;
         }
-        setPrivacyScreenEnabled(defValue);
-      }
-    }
-    return defValue;
-  }
+        try {
+            return preferences.getBoolean(KEY_PRIVACY_SCREEN_ENABLED, defValue);
+        } catch (ClassCastException e) {
+            Log.w("PrivacyScreen", "SharedPreference '" + KEY_PRIVACY_SCREEN_ENABLED + "' was not a Boolean value.", e);
 
-  /**
-   * Converts the entry from a {@link String} to {@link Boolean} if possible.
-   *
-   * @return true if the entry was a {@code String} value that could be resolved to a {@code Boolean} value, false otherwise.
-   * @see #isPrivacyScreenEnabled(boolean, boolean)
-   * @see #KEY_PRIVACY_SCREEN_ENABLED
-   */
-  private boolean convertStringEntryToBoolean() {
-    try {
-      String val = preferences.getString(KEY_PRIVACY_SCREEN_ENABLED, null);
-      if (val == null) {
+            if (shouldCorrect) {
+                if (convertStringEntryToBoolean()) {
+                    return preferences.getBoolean(KEY_PRIVACY_SCREEN_ENABLED, defValue);
+                }
+                setPrivacyScreenEnabled(defValue);
+            }
+        }
+        return defValue;
+    }
+
+    /**
+     * Converts the entry from a {@link String} to {@link Boolean} if possible.
+     *
+     * @return true if the entry was a {@code String} value that could be resolved to a {@code Boolean} value, false otherwise.
+     * @see #isPrivacyScreenEnabled(boolean, boolean)
+     * @see #KEY_PRIVACY_SCREEN_ENABLED
+     */
+    private boolean convertStringEntryToBoolean() {
+        try {
+            String val = preferences.getString(KEY_PRIVACY_SCREEN_ENABLED, null);
+            if (val == null) {
+                return false;
+            }
+            if (val.equalsIgnoreCase("true")) {
+                setPrivacyScreenEnabled(true);
+                return true;
+            } else if (val.equalsIgnoreCase("false")) {
+                setPrivacyScreenEnabled(false);
+                return true;
+            }
+        } catch (ClassCastException e) {
+            Log.w("PrivacyScreen", "SharedPreference '" + KEY_PRIVACY_SCREEN_ENABLED + "' was not a String value.", e);
+        }
         return false;
-      }
-      if (val.equalsIgnoreCase("true")) {
-        setPrivacyScreenEnabled(true);
-        return true;
-      } else if (val.equalsIgnoreCase("false")) {
-        setPrivacyScreenEnabled(false);
-        return true;
-      }
-    } catch (ClassCastException e) {
-      Log.w("PrivacyScreen", "SharedPreference '" + KEY_PRIVACY_SCREEN_ENABLED + "' was not a String value.", e);
     }
-    return false;
-  }
 
-  /**
-   * Sets the value of the Privacy Screen Enabled entry in {@link SharedPreferences}.
-   *
-   * @param value the value to set.
-   * @return true if successful, false otherwise.
-   * @see #KEY_PRIVACY_SCREEN_ENABLED
-   */
-  private boolean setPrivacyScreenEnabled(boolean value) {
-    preferences.edit().putBoolean(KEY_PRIVACY_SCREEN_ENABLED, value).apply();
-    return true;
-  }
-
-  private boolean actualyChangeFlag(){
-	  
-	  if (preferences.getBoolean(KEY_PRIVACY_SCREEN_ENABLED, true)) 
-	  {
-	  setPrivacyScreenEnabled(true);
-		this.activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
-		return true;
-	  } else
-	  {
-		setPrivacyScreenEnabled(false);
-		this.params.flags &= ~WindowManager.LayoutParams.FLAG_SECURE; 
-		this.wm.updateViewLayout(currView, this.params);
-		return true;
-	  }
-	
-  }
-
-  @Override
-  public Object onMessage(String id, Object data) {
-    if (id == "isPrivacyScreenEnabled") {
-      boolean shouldCorrect = (data instanceof Boolean) && (data != null) && ((Boolean) data);
-      return isPrivacyScreenEnabled(true, shouldCorrect);
-    } else if (id == "setPrivacyScreenEnabled") {
-      if (data instanceof Boolean && data != null) {
-        return setPrivacyScreenEnabled((Boolean) data);
-      }
-      return false;
+    /**
+     * Sets the value of the Privacy Screen Enabled entry in {@link SharedPreferences}.
+     *
+     * @param value the value to set.
+     * @return true if successful, false otherwise.
+     * @see #KEY_PRIVACY_SCREEN_ENABLED
+     */
+    private boolean setPrivacyScreenEnabled(boolean value) {
+        preferences.edit().putBoolean(KEY_PRIVACY_SCREEN_ENABLED, value).apply();
+        return true;
     }
-    return null;
-  }
+
+    private boolean actualyChangeFlag() {
+
+        if (preferences.getBoolean(KEY_PRIVACY_SCREEN_ENABLED, true)) {
+            setPrivacyScreenEnabled(true);
+            /*this.activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);*/
+            return true;
+        } else {
+            setPrivacyScreenEnabled(false);
+           /* this.params.flags &= ~WindowManager.LayoutParams.FLAG_SECURE;
+            this.wm.updateViewLayout(currView, this.params);*/
+            return true;
+        }
+
+    }
+
+    @Override
+    public Object onMessage(String id, Object data) {
+        if (id == "isPrivacyScreenEnabled") {
+            boolean shouldCorrect = (data instanceof Boolean) && (data != null) && ((Boolean) data);
+            return isPrivacyScreenEnabled(true, shouldCorrect);
+        } else if (id == "setPrivacyScreenEnabled") {
+            if (data instanceof Boolean && data != null) {
+                return setPrivacyScreenEnabled((Boolean) data);
+            }
+            return false;
+        }
+        return null;
+    }
 }
